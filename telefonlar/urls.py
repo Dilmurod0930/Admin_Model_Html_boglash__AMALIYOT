63b233f1de2a49b679ebec8acf606d3fe8b6730c
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-02 20:52:02 +0500

    Prayekt  yakunlandi

--- telefonlar/urls.py
+++ telefonlar/urls.py
@@ -4,5 +4,5 @@ from .views import  home ,  telefon_list, telefon_info
 urlpatterns = [
     path('', home,  name='home'),
     path('telefon_list', telefon_list, name='telefon_list'),
-    path('telefon<int:pk>/', telefon_info, name='telefon_info'),
+    path('telefon/<int:id>', telefon_info, name='telefon_info'),
 ]
--- telefonlar/views.py
+++ telefonlar/views.py
@@ -6,11 +6,11 @@ def home(request):
     return render(request, 'home.html')
 
 def telefon_list(request):
-    telefons = Telefon.objects.all()
-    context = {'telefons': telefons}
-    return render(request, 'telefons/telefon_list.html',  context= context)
+    tele = Telefon.objects.all()
+    context = {'tele': tele}
+    return render(request, 'telefon/telefon_list.html',  context= context)
 
-def  telefon_info(request,  pk):
-    telefons =Telefon.objects.get(id=pk)
-    context = {'telefons': telefons}
-    return render(request, 'telefonlar/telefon_info.html', context = context)
+def  telefon_info(request,  id):
+    tele = Telefon.objects.get(id=id)
+    context = {'tele': tele}
+    return render(request, 'telefon/telefon_info.html', context = context)
