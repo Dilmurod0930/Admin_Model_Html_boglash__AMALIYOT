1e35647c6db80b39dd8d583bd619b044dca63e83
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-02 20:09:20 +0500

    Main  page  Tayyor  Seetings  htmlga to'g'irlandi  TEMPLATES  papkaga  rusxat  berildi

--- telefon_bozor/settings.py
+++ telefon_bozor/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'telefon_bozor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- telefonlar/urls.py
+++ telefonlar/urls.py
@@ -2,7 +2,7 @@ from  django.urls import  path
 from .views import  home ,  telefon_list, telefon_info
 
 urlpatterns = [
-    path('', telefon_list,  name='telefon_list'),
+    path('', home,  name='home'),
     path('telefon_list', telefon_list, name='telefon_list'),
     path('telefon<int:pk>/', telefon_info, name='telefon_info'),
 ]
