8f70951495d2918e845548d5c84b173db05c76bb
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-02 18:18:08 +0500

    App  ichigadi  views.py  faylga  malumotlar  yozildi.

--- telefonlar/views.py
+++ telefonlar/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def telefon_list(request):
+    return render(request, 'telefonlar/telefon_list.html')
+
+def  telefon_info(request):
+    return render(request, 'telefonlar/telefon_info.html')
