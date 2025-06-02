1bca1e821af7c58a41a8178b17b89c3fca4cd427
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-02 19:57:46 +0500

    views.py  fayl  to'g'irlandi

--- telefonlar/views.py
+++ telefonlar/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import Telefon
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def telefon_list(request):
-    return render(request, 'telefonlar/telefon_list.html')
+    telefons = Telefon.objects.all()
+    context = {'telefons': telefons}
+    return render(request, 'telefonlar/telefon_list.html',  context= context)
 
-def  telefon_info(request):
-    return render(request, 'telefonlar/telefon_info.html')
+def  telefon_info(request,  pk):
+    tele =Telefon.objects.get(id=pk)
+    context = {'tele': tele}
+    return render(request, 'telefonlar/telefon_info.html', context = context)
